9b04811bf4b3262605fab3244ffce5437fe9f40d
erDaren <gangqiaoshu@qq.com>
2016-11-23 11:56:40 +0800

    continue trying

--- app/src/main/java/com/sanmiao/bluetoothdemo/utils/Constants.java
+++ app/src/main/java/com/sanmiao/bluetoothdemo/utils/Constants.java
@@ -10,13 +10,13 @@ import java.util.UUID;
 
 public class Constants {
     //    cmd
-    public static final String CMD_TURN_ON = "";
-    public static final String CMD_TURN_OFF = "";
-    public static final String CMD_QUERY = "";
+    public static final String CMD_TURN_ON = "7e000201292713557f";
+    public static final String CMD_TURN_OFF = "7e000200292713557f";
+    public static final String CMD_QUERY = "7e000101292713557f";
 
     //  uuid
-    public static final UUID UUID_CONNECT = UUID.fromString("");
-    public static final UUID UUID_SEND_CMD = UUID.fromString("");
+    public static final UUID UUID_CONNECT = UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
+    public static final UUID UUID_SEND_CMD = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
 
     //  cmd interval
     public static final long CMD_INTERVAL = 100;
